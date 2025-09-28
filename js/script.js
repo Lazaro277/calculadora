0fe81f7b3b8178c2612358e2ee78648fa3973854
Lázaro <lazaromessias101@gmail.com>
2025-09-28 14:11:12 -0300

    Inicial

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function calcular(tipo, valor) {
+  if (tipo === 'acao') {
+    if (valor === 'c') {
+      //limpar o visor
+      document.getElementById('resultado').value = '';
+      document.getElementById('resultado2').value = '';
+      document.getElementById('previa_resultado').value = '';
+    }
+    if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+      document.getElementById('resultado').value += valor;
+      document.getElementById('resultado2').value += valor;
+      document.getElementById('previa_resultado').value += valor;
+    }
+    if (valor === '=') {
+      var valor_campo = eval(document.getElementById('resultado').value);
+      document.getElementById('resultado').value = valor_campo;
+      document.getElementById('resultado2').classList.add('pt-2');
+      document.getElementById('resultado2').value = valor_campo;
+      document.getElementById('previa_resultado').value += '=';
+      document.getElementById('previa_resultado').classList.remove('d-none');
+      document.getElementById('previa_resultado').classList.add('d-block');
+    }
+    if (valor === 'apagar') {
+      var valorAtual = document.getElementById('resultado2').value;
+      if (valorAtual.length > 0) {
+        document.getElementById('resultado2').value = valorAtual.slice(0, -1);
+      }
+    }
+  } else if (tipo === 'valor') {
+    document.getElementById('resultado').value += valor;
+    document.getElementById('resultado2').value += valor;
+    document.getElementById('previa_resultado').value += valor;
+  }
+}
+function trocarEstilo(estilo1, estilo2) {
+  document.getElementById(estilo1).classList.remove('d-none');
+  document.getElementById(estilo1).classList.add('d-block');
+  document.getElementById(estilo2).classList.add('d-none');
+  document.getElementById(estilo2).classList.remove('d-block');
+}
