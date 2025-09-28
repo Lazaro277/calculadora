2bf85e11c7d0cc0c36550650975071afac2df85b
Lázaro <lazaromessias101@gmail.com>
2025-09-28 15:25:20 -0300

    Correções de bugs

--- js/script.js
+++ js/script.js
@@ -5,26 +5,31 @@ function calcular(tipo, valor) {
       document.getElementById('resultado').value = '';
       document.getElementById('resultado2').value = '';
       document.getElementById('previa_resultado').value = '';
+      document.getElementById('previa_resultado').classList.remove('d-block');
+      document.getElementById('previa_resultado').classList.add('d-none');
     }
     if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
       document.getElementById('resultado').value += valor;
       document.getElementById('resultado2').value += valor;
       document.getElementById('previa_resultado').value += valor;
     }
+    if (valor === 'apagar') {
+      var valorAtual = document.getElementById('resultado2').value;
+      var valorAtual2 = document.getElementById('previa_resultado').value;
+      if (valorAtual.length > 0) {
+        document.getElementById('resultado2').value = valorAtual.slice(0, -1);
+        document.getElementById('previa_resultado').value = valorAtual2.slice(0, -1);
+      }
+    }
     if (valor === '=') {
       var valor_campo = eval(document.getElementById('resultado').value);
+      var valor_campo2 = eval(document.getElementById('resultado2').value);
       document.getElementById('resultado').value = valor_campo;
       document.getElementById('resultado2').classList.add('pt-2');
-      document.getElementById('resultado2').value = valor_campo;
+      document.getElementById('resultado2').value = valor_campo2;
       document.getElementById('previa_resultado').value += '=';
       document.getElementById('previa_resultado').classList.remove('d-none');
       document.getElementById('previa_resultado').classList.add('d-block');
-    }
-    if (valor === 'apagar') {
-      var valorAtual = document.getElementById('resultado2').value;
-      if (valorAtual.length > 0) {
-        document.getElementById('resultado2').value = valorAtual.slice(0, -1);
-      }
     }
   } else if (tipo === 'valor') {
     document.getElementById('resultado').value += valor;
